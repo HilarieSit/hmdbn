fixed posterior initialization and numerical issues

baum_welch.py
@@ -1,4 +1,6 @@
 import numpy as np
+import inspect
+import traceback
 
 '''
 Calculate sum of log probabilities for two values/arrays
@@ -11,12 +13,19 @@ def sumLogProbs(a, b):
     " function for calculating sumLogProbs for two values/arrays (vectorized)"
     b_a = np.expand_dims(a+np.log(1+np.exp(b-a)), axis=0)
     a_b = np.expand_dims(b+np.log(1+np.exp(a-b)), axis=0)
+    curframe = inspect.currentframe()
+    calframe = inspect.getouterframes(curframe, 2)
+    if np.isnan(np.sum(b_a)) or np.isnan(np.sum(a_b)):
+        print(a)
+        print(b)
+        print('caller name:', calframe[2][3])
+        traceback.print_stack()
     return np.squeeze(np.amax(np.concatenate((b_a, a_b), axis=0), axis=0))
 
 '''
 Calculate sum of log probabilities for list of values/arrays
 Arguments:
-    args: list of log probabilities to sum
+    args: list of log probabilities toc sum
 Returns:
 	sum of log probabilities
 '''
@@ -30,6 +39,17 @@ def sumLogProbsFunc(args):
             sumtotal = sumLogProbs(sumtotal, term)
         return sumtotal
 
+def log2norm_dicts(some_dict):
+    for key, n_dict in some_dict.items():
+        for key2, val in n_dict.items():
+            some_dict[key][key2] = np.exp(val)
+
+    return some_dict
+        
+
+    
+
+
 '''
 Return observations for all parents at specified time 
 Arguments:
@@ -41,8 +61,8 @@ Returns:
 	parent observations
 '''
 def get_parent_obs(current_gene, timeseries, parents, time):
-    if not parents:
-        parents = [current_gene]
+    # if not parents:
+    #     parents = [current_gene]
     parent_obs = str([timeseries.get(parent)[time] for parent in parents])
     return parent_obs
 
@@ -52,7 +72,7 @@ Arguments:
 	obs [tuple]: observed sequence of emitted states for gene_i, timeseries dictionary 
 	probs [tuple]: initial, emission, transition probability dicts
 Returns:
-	F [float]: matrix of forward probabilities
+	F [float]: matrix of forward  probabilities
 	B [float]: matrix of backward probabilities
 	R [float]: matrix of posterior probabilities
     likelihood_f [float]: P(obs) calculated using the forward algorithm
@@ -85,6 +105,12 @@ def forward_backward(child_gene, obs, states, probs):
                     F_list.append(F[prev_q,i-1] + trans_probs[prev_q][q])
                     B_list.append(trans_probs[q][prev_q] + emiss_probs[prev_q][current_obs[T-i]][back_parent_obs] + B[prev_q, T-i])
                 F_sum_term = sumLogProbsFunc(F_list)
+                if np.isnan(F_sum_term):
+                    print(F)
+                    print(log2norm_dicts(trans_probs))
+                    print(emiss_probs)
+                    print(F_list)
+                    exit()
                 F[q, i] = emiss_probs[q][current_obs[i]][for_parent_obs] + F_sum_term
                 B[q, T-i-1] = sumLogProbsFunc(B_list)
     

hmdbn.py
@@ -41,11 +41,11 @@ Returns:
 '''
 def identify_parent_emissions(state, ri):
     state_parents = state.parents
-    if not state_parents:
-        # if '[]', parent is itself
-        cp_length = 1
-    else:
-        cp_length = len(state_parents)
+    # if not state_parents:
+    #     # if '[]', parent is itself
+    #     cp_length = 1
+    # else:
+    cp_length = len(state_parents)
     combinations = [list(vals) for vals in itertools.product(ri, repeat=cp_length)]
 
     chi_dict = {}
@@ -111,15 +111,24 @@ def identify_states(node_parents, P_list, T):
     # search for unique states
     states = [] 
     [states.append(x) for x in state_list if x not in states]
-
+    
     # initialize P matrix
-    P = np.zeros([len(states), T])
+    P = np.ones([len(states), T])
     pt = 0
-    for state, tt in zip(state_list, transition_times):
-        p_ind = states.index(state)
-        P[p_ind, pt:tt] = 1
-        pt = tt
+    print('node parents', node_parents)
+    print(transition_times)
+    for state_id, state in enumerate(states):
+        for i, sP in enumerate(P_list):
+            parent = node_parents[i]
+            if parent in state:
+                P[state_id, :] *= sP[0, :]
+            else:
+                P[state_id, :] *= sP[1, :]
 
+        # normalize P so that it sums to 1
+    P_denom = np.tile(np.sum(P, axis=0), (len(states), 1))
+    P = P/P_denom
+    # plot_puntative_graphs2(node_parents, P_list, states, P)
     return states, P, n_seg
 
 '''
@@ -209,6 +218,10 @@ Returns:
 def structural_EM(timeseries, node_i, node_lib, init_posteriors=None, initialization=False):
     child_gene = node_i.gene
     current_obs = timeseries.get(child_gene)[1:]
+    # vals, counts = np.unique(current_obs, return_counts=True)
+    # P = counts/np.sum(counts)
+    # print(P)
+
     obs = (current_obs, timeseries)
     T = len(current_obs)
     ri = np.unique([all_obs for all_obs in timeseries.values()])
@@ -227,7 +240,6 @@ def structural_EM(timeseries, node_i, node_lib, init_posteriors=None, initializa
                 other_nodes = list(node_lib.keys())
                 other_nodes.remove(child_gene)
                 for parent in parents:
-                    print(parent)
                     other_nodes.remove(parent)
                 # add random parent
                 parent_gene = np.random.choice(other_nodes)          
@@ -237,15 +249,15 @@ def structural_EM(timeseries, node_i, node_lib, init_posteriors=None, initializa
 
             # identify putative hidden graphs from stationary network
             states, state_emiss, chi_dicts, P, n_seg = putative_hidden_graphs(timeseries, node_i, node_lib, init_posteriors, T, ri)  
-            plot_posteriors(P, states)
+            print(states)
 
             if len(node_i.parents) == 1:
-                n_seg = None
+                n_seg = 1
                 # return bwbic_score from pre-initialization
 
         else:
             # initialization step with single parent 
-            no_parent_node = node(current_gene, [current_gene])         # no parents - parent is itself
+            no_parent_node = node(current_gene, [])
             states = [node_i, no_parent_node]
             state_emiss, chi_dicts = [], []
             for state in states:
@@ -254,7 +266,7 @@ def structural_EM(timeseries, node_i, node_lib, init_posteriors=None, initializa
                 chi_dicts.append(chi_dict)
             # initialize posterior as 50-50
             P = np.ones([2, T])/2
-            n_seg = None
+            n_seg = 1
     
         print('parents: ', [parents for parents in node_i.parents])
 
@@ -283,8 +295,8 @@ def structural_EM(timeseries, node_i, node_lib, init_posteriors=None, initializa
 
             prev_likelihood = likelihood
 
-        if initialization is False:
-            plot_posteriors(P, states)
+        # if initialization is False:
+        #     plot_posteriors(P, states)
 
         if initialization:
             return P, bwbic_score
@@ -324,7 +336,7 @@ if __name__ == "__main__":
     all_genes = list(gene_id.keys())
     timeseries = load_data(gene_id, 'data/testing')
     node_lib = initialize_nodes(all_genes)
-    current_gene = 'actn'
+    current_gene = 'up'
     print('\n\033[1mCURRENT GENE: '+current_gene+'\033[0m')
 
     # preinitialize 
@@ -333,12 +345,9 @@ if __name__ == "__main__":
 
 
 
-
-
-
     # construct list of all nodes (corresponding to genes) & position dict
 
-    gene = 'up'
+    gene = 'twi'
     
     # perform structural EM on every gene
 

probs_update.py
@@ -25,12 +25,9 @@ def initialize_prob_dicts(state_emiss, ri, T, n_seg):
     Ri = len(ri)
     n_configs = len(state_emiss)
     pi_init = np.log(1/n_configs)
-    if n_seg is None:
-        trans_prob_init = np.log(0.05)
-        self_prob_init = np.log(0.95)
-    else:
-        trans_prob_init = np.log(1/10)
-        self_prob_init = np.log(1-(n_seg/10))
+
+    trans_prob_init = np.log(1/65)
+    self_prob_init = np.log(1-(n_seg/65))
 
     for state_id, combinations in enumerate(state_emiss):
         init_probs[state_id] = pi_init
@@ -44,7 +41,7 @@ def initialize_prob_dicts(state_emiss, ri, T, n_seg):
         for gene_emiss in range(Ri):
             for parent_emiss in combinations:
                 emiss_probs[state_id][gene_emiss][str(parent_emiss)] = 0  
-                    
+
     return trans_probs, emiss_probs, init_probs
 
 '''
@@ -83,6 +80,10 @@ def update_probs(current_gene, obs, states, probs, F, B, P, f_likelihood):
                 
                 # calculate numerator 
                 A_count = F[prev_q,t-1]+trans_probs[prev_q][q]+emiss_probs[q][current_obs[t]][back_parent_obs]+B[q,t]-f_likelihood
+
+                if np.isnan(np.sum(F)):
+                    print(F)
+                    print(f_likelihood)
                 
                 if t == 0:
                     A_num[prev_q][q] = A_count
@@ -97,7 +98,10 @@ def update_probs(current_gene, obs, states, probs, F, B, P, f_likelihood):
 
         # calculate transition probability
         for q2 in range(len(states)):
-            trans_probs[prev_q][q2] = A_num[prev_q][q2]-A_denom
+            prob = A_num[prev_q][q2]-A_denom
+            if np.isnan(prob):
+                prob = 1
+            trans_probs[prev_q][q2] = prob
 
     return init_probs, trans_probs
 
@@ -149,25 +153,35 @@ def calculate_theta(current_gene, obs, states, state_emiss, chi_dicts, emiss_pro
         # fill in emiss_probs
         for gene_emiss in range(Ri):
             for parent_emiss in combinations:
-                emiss_probs[state_id][gene_emiss][str(parent_emiss)] = theta_matrix[gene_emiss, chi_dict.get(str(parent_emiss))]-np.log(len(combinations))
+                # emiss_probs[state_id][gene_emiss][str(parent_emiss)] = theta_matrix[gene_emiss, chi_dict.get(str(parent_emiss))]-np.log(len(combinations))
+                emiss_probs[state_id][gene_emiss][str(parent_emiss)] = theta_matrix[gene_emiss, chi_dict.get(str(parent_emiss))]+np.log(0.5)
                 if emiss_probs[state_id][gene_emiss][str(parent_emiss)] == -inf:
                     emiss_probs[state_id][gene_emiss][str(parent_emiss)] = 1
 
-        bwbic_first_term = np.zeros((Ri, Gi))
+        # bwbic_first_term = np.zeros((Ri, Gi))
+        # for t in range(T): 
+        #     # from current_val & parent_vals, identify where to put count in chi (knonecker delta)
+        #     current_val = current_obs[t]
+        #     parent_vals = get_parent_obs(current_gene, timeseries, s_parents, t)
+        #     chi_index = chi_dict.get(str(parent_vals))
+        #     bwbic_first_term[current_val, chi_index] += P[state_id, t] * theta_matrix[current_val, chi_index]
+        
+        # bwbic_first_term = np.sum(bwbic_first_term)
+        # bwbic_second_term = (Gi/2)*(Ri-1)*np.log(np.sum(P[state_id,:]))
+        # bwbic = bwbic_first_term-bwbic_second_term
+        # bwbic_score.append(bwbic)
+
+        bwbic_first_term = 0
         for t in range(T): 
             # from current_val & parent_vals, identify where to put count in chi (knonecker delta)
             current_val = current_obs[t]
             parent_vals = get_parent_obs(current_gene, timeseries, s_parents, t)
             chi_index = chi_dict.get(str(parent_vals))
-            bwbic_first_term[current_val, chi_index] += P[state_id, t] * theta_matrix[current_val, chi_index]
+            bwbic_first_term += P[state_id, t] * theta_matrix[current_val, chi_index]
         
-        bwbic_first_term = np.sum(bwbic_first_term)
         bwbic_second_term = (Gi/2)*(Ri-1)*np.log(np.sum(P[state_id,:]))
         bwbic = bwbic_first_term-bwbic_second_term
         bwbic_score.append(bwbic)
-    print([state.parents for state in states])
+    bwbic_score = np.sum(bwbic_score)
     print(bwbic_score)
-    bwbic_score = np.mean(bwbic_score)
     return theta_cond, emiss_probs, bwbic_score
-
-# def calculate_bwbic(theta_cond, chi_dict, timeseries, config):